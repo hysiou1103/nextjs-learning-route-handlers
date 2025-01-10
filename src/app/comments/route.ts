2412a69ca8edd539bb687b5b2f09a3018895e68e
Molly.Lin <hysiou1103@gmail.com>
2025-01-10 15:07:34 +0800

    Tutorial35-Handling POST Request

--- src/app/comments/route.ts
+++ src/app/comments/route.ts
@@ -4,3 +4,24 @@ import { comments } from './data';
 export async function GET() {
   return Response.json(comments);
 }
+
+// 使用 Thunder Client 這個 extension 來協助測試 API，可以先打 GET 請求，再打 POST 請求，最後再打 GET 請求，就可以看到新增的評論
+export async function POST(request: Request) {
+  // 從請求中提取 JSON 主體
+  const comment = await request.json();
+
+  // 創建新的評論物件
+  const newcomment = {
+    id: comments.length + 1,
+    text: comment.text,
+  };
+
+  // 將新評論加入 comments 陣列
+  comments.push(newcomment);
+
+  // 回應新增的評論，並設定狀態碼為 201
+  return new Response(JSON.stringify(newcomment), {
+    headers: { 'Content-Type': 'application/json' },
+    status: 201,
+  });
+}
