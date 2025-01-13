048c8fe8d6b7b263a96180ffdfb6160837460c6a
Molly.Lin <hysiou1103@gmail.com>
2025-01-13 15:22:03 +0800

    Tutorial42-Cookies in Route Handlers

--- src/app/profile/api/route.tsx
+++ src/app/profile/api/route.tsx
@@ -4,7 +4,12 @@ export async function GET(request: NextRequest) {
   const requestHeader = request.headers; // 取得 request 的 headers
   console.log(requestHeader.get('Authorization')); // 取得 header 中 Authorization 的值
 
+  const theme = request.cookies.get('theme'); // 取得 cookie
+
   return new Response('<h1>Profile API data</h1>', {
-    headers: { 'Content-Type': 'text/html' }, // 設置 response header
+    headers: {
+      'Content-Type': 'text/html', // 設置 response header
+      'Set-cookie': 'theme=dark', // 設置 cookie
+    },
   });
 }
