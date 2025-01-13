a6b54f93b2d56caddb0cdda13d1e36687d58dcd4
Molly.Lin <hysiou1103@gmail.com>
2025-01-13 12:08:21 +0800

    Tutorial40-Redirects： 1. 依官方文件修正，在使用 params 的屬性前須 awaited 2. redirect 用法

--- src/app/comments/[id]/route.ts
+++ src/app/comments/[id]/route.ts
@@ -1,4 +1,6 @@
 import { comments } from '../data';
+import { redirect } from 'next/navigation';
+
 // 資料夾名稱中使用方括號 [ ] 來定義動態部分
 // 第二個參數為 context，從裡面解構出 params 物件，該物件內包含動態路由的參數，這是一種常見的開發慣例，並且可以避免 ESLint 報錯，ex: _request
 export async function GET(
@@ -6,9 +8,14 @@ export async function GET(
   _request: Request,
   { params }: { params: { id: string } }
 ) {
-  const comment = comments.find(
-    (comment) => comment.id === parseInt(params.id)
-  );
+  const { id } = await params;
+
+  // 當輸入 url /comments/4時，就會重新導向到 /comments，從 network 觀察會發現狀態碼為 307
+  if (parseInt(params?.id) > comments.length) {
+    return redirect('/comments');
+  }
+
+  const comment = comments.find((comment) => comment.id === parseInt(id));
   return Response.json(comment);
 }
 
@@ -18,10 +25,9 @@ export async function PATCH(
 ) {
   const body = await request.json();
   const { text } = body;
+  const { id } = await params;
 
-  const index = comments.findIndex(
-    (comment) => comment.id === parseInt(params.id)
-  );
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
   comments[index].text = text; // 更新評論
   return Response.json(comments[index]); // return updated comments
 }
@@ -30,9 +36,8 @@ export async function DELETE(
   _request: Request,
   { params }: { params: { id: string } }
 ) {
-  const index = comments.findIndex(
-    (comment) => comment.id === parseInt(params.id)
-  );
+  const { id } = await params;
+  const index = comments.findIndex((comment) => comment.id === parseInt(id));
   const delectedComment = comments[index];
   comments.splice(index, 1); // 刪除評論
   return Response.json(delectedComment); // return deleted comment
